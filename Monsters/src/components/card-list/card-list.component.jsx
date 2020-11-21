97f0bab6e53d8bdb5660f74e417dfa1ef34e393e
AkshayGarg <akshay.garg@sevenlakes.com>
2020-11-22 00:38:34 +0530

    first commit

---
+++ Monsters/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./card-list.css";
+import { Card } from "../card/card.component";
+
+export const CardList = (prop) => (
+  <div className="card-list">
+    {prop.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
